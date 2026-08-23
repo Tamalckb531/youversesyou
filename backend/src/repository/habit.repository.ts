9a68fb343c38233d0b70d6e724fd8cdb01f60372
Tamal <ckbtamaldipnew@gmail.com>
2026-08-23 09:11:34 +0600

    Changes

--- backend/src/repository/habit.repository.ts
+++ backend/src/repository/habit.repository.ts
@@ -3,7 +3,7 @@ import { getDb } from "../db";
 import { habits } from "../db/schema";
 
 export const HabitRepository = {
-    async findAlHabitsByUserId(userId: string) {
+    async findAllHabitsByUserId(userId: string) {
         return await getDb()
             .select()
             .from(habits)
