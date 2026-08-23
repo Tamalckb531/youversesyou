4874d6fd5f277f06bac55861d6f9e057c275af0e
Tamal <ckbtamaldipnew@gmail.com>
2026-08-23 07:37:20 +0600

    add controller design

---
+++ backend/src/controller/habit.controller.ts
@@ -0,0 +1,8 @@
+import type { Context } from "hono";
+
+export const HabitGetController = async (c: Context) => { }
+export const HabitGetOneController = async (c: Context) => { }
+export const HabitCreateController = async (c: Context) => { }
+export const HabitPatchController = async (c: Context) => { }
+export const HabitMarkController = async (c: Context) => { }
+export const HabitUnMarkController = async (c: Context) => { }
