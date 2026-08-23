00b181a19ed43e75cafe7484c4e1e5c6330ea3bf
Tamal <ckbtamaldipnew@gmail.com>
2026-08-23 09:11:14 +0600

    add findAllHabitsByUserId in HabitRepository

--- backend/src/repository/habit.repository.ts
+++ backend/src/repository/habit.repository.ts
@@ -3,5 +3,10 @@ import { getDb } from "../db";
 import { habits } from "../db/schema";
 
 export const HabitRepository = {
-
+    async findAlHabitsByUserId(userId: string) {
+        return await getDb()
+            .select()
+            .from(habits)
+            .where(eq(habits.userId, userId));
+    },
 }
