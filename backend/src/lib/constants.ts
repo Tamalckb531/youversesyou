7efb417c9a83a8015fc2c2c108cc031c8fea22d6
Tamal <ckbtamaldipnew@gmail.com>
2026-08-24 20:17:25 +0600

    add TodoCreateController

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -35,7 +35,7 @@ export const responseMsg = {
         success: {
             GET_ALL: "Habits loaded",
             GET_ONE :"Single habit loaded",
-            CREATED_BULK: "Habits created successfully",
+            CREATED_BULK: "Habit created successfully",
             UPDATE_ONE :"Habit updated successfully",
             MARKED :"Habit marked successfully",
             UNMARKED :"Habit unmarked successfully",
@@ -52,7 +52,7 @@ export const responseMsg = {
         success: {
             GET_ALL: "Todos loaded",
             GET_ONE :"Single todo loaded",
-            CREATED_BULK: "Todos created successfully",
+            CREATED_BULK: "Todo created successfully",
             UPDATE_ONE :"Todo updated successfully",
             MARKED :"Todo marked successfully",
             UNMARKED :"Todo unmarked successfully",
