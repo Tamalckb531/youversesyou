24362376148a754f99de79a7cb620848d3eae01d
Tamal <ckbtamaldipnew@gmail.com>
2026-08-24 20:15:02 +0600

    add TodoGetOneController

--- backend/src/controller/todo.controller.ts
+++ backend/src/controller/todo.controller.ts
@@ -30,6 +30,37 @@ export const TodoGetController = async (c: Context) => {
 }
 
 export const TodoGetOneController = async (c: Context) => { 
+    try {
+        const id = c.req.param("id");
+        const user = c.get("user");
+        const userId = user.id;
+
+        if (!id) return c.json({
+            success: false,
+            msg: responseMsg.todo.error.NO_TODO_ID,
+            data: null
+        }, 400);
+        if (!userId) return c.json({
+            success: false,
+            msg: responseMsg.generic.error.NO_USER_ID,
+            data: null
+        }, 400);
+
+        const item = await TodoService.oneTodo(userId, id);
+        
+        return c.json({
+            success: true,
+            msg:responseMsg.todo.success.GET_ONE,
+            data: item,
+        })
+    }
+    catch (err) {
+        return c.json({
+            success: false,
+            msg: err instanceof Error ? err.message : responseMsg.generic.error.GENERIC_500,
+            data:null
+        }, 500); 
+    }
 }
 
 export const TodoCreateController = async (c: Context) => { 
