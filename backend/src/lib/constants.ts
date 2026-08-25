cf9081e774b1e3158a32ef7a8c85798477aac60b
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:21:11 +0600

    add createOne

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -61,7 +61,8 @@ export const responseMsg = {
         error: {
             INVALID_BODY: "Invalid request body",
             NO_TODO_ID: "No todo detected",
-            INVALID_LINK_ID: "linked id do not exist or do not belong to you",
+            INVALID_PLAN_ID: "This plan id link for todo do not exist or do not belong to you",
+            INVALID_HABIT_ID: "This habit id link for todo do not exist or do not belong to you",
         },
     },
     generic: {
--- backend/src/service/todo.service.ts
+++ backend/src/service/todo.service.ts
@@ -1,4 +1,6 @@
 import { responseMsg } from "../lib/constants";
+import { HabitRepository } from "../repository/habit.repository";
+import { PlanRepository } from "../repository/plan.repository";
 
 export const TodoService = {
     async allTodos(userId: string) {
@@ -9,5 +11,20 @@ export const TodoService = {
         const todo = await TodoRepository.oneTodo(habitId, userId);
         if (!todo) throw new Error(responseMsg.todo.error.NO_TODO_ID);
         return todo;
+    },
+
+    async createOne(userId: string, item: TodoCreateItem) {
+        let plan = null;
+        let habit = null;
+        
+        if(item.planId) plan = await PlanRepository.findOnePlanByUserIdWithoutCon(userId, item.planId);
+        if (item.habitId) habit = await HabitRepository.findOneHabitByUserIdWithoutCon(userId, item.habitId);
+        
+        if (item.planId && !plan) throw new Error(responseMsg.todo.error.INVALID_PLAN_ID)
+        if (item.habitId && !habit) throw new Error(responseMsg.todo.error.INVALID_HABIT_ID)
+        
+        const newItem = toNewTodo(item)
+        
+        return TodoRepository.createWithLinks(userId, newItem)
     },
 }
