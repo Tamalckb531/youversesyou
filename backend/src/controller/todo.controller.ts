8e6f91e5fb64457bb736074b9923e83ddfb04832
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:04:06 +0600

    add TodoDeleteController controller

--- backend/src/controller/todo.controller.ts
+++ backend/src/controller/todo.controller.ts
@@ -161,6 +161,41 @@ export const TodoPatchController = async (c: Context) => {
 }
 
 export const TodoDeleteController = async (c: Context) => { 
+    try {
+        const id = c.req.param("id");
+
+        const user = c.get("user");
+        const userId = user.id;
+
+        if (!id) return c.json({
+            success: false,
+            msg: responseMsg.todo.error.NO_TODO_ID,
+            data: null
+        }, 400);
+        if (!userId) return c.json({
+            success: false,
+            msg: responseMsg.generic.error.NO_USER_ID,
+            data: null
+        }, 400);
+
+        const deletedHabitId = await TodoService.deleteTodo(userId, id);
+
+        return c.json({
+            success: true,
+            msg: responseMsg.todo.success.DELETED,
+            data: deletedHabitId
+        }, 201);
+    }
+    catch (err) {
+        return c.json(
+            {
+                success: false,
+                msg: err instanceof Error ? err.message : responseMsg.generic.error.GENERIC_500, 
+                data: null
+            },
+            500,
+        );
+    }
 }
 
 export const TodoMarkController = async (c: Context) => { 
