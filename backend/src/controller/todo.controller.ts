0136d545c36122213c8cb4e62a97191574fdbe22
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:08:35 +0600

    add TodoMarkController

--- backend/src/controller/todo.controller.ts
+++ backend/src/controller/todo.controller.ts
@@ -199,4 +199,39 @@ export const TodoDeleteController = async (c: Context) => {
 }
 
 export const TodoMarkController = async (c: Context) => { 
+    try {
+        const id = c.req.param("id");
+
+        const user = c.get("user");
+        const userId = user.id;
+
+        if (!id) return c.json({
+            success: false,
+            msg: responseMsg.todo.error.NO_TODO_ID,
+            data: null
+        }, 400);
+        if (!userId) return c.json({
+            success: false,
+            msg: responseMsg.generic.error.NO_USER_ID,
+            data: null
+        }, 400);
+
+        const mark = await TodoService.markedTodo(userId, id);
+
+        return c.json({
+            success: true,
+            msg: mark.marked ? responseMsg.todo.success.MARKED : responseMsg.todo.success.UNMARKED,
+            data: mark
+        }, 201);
+    }
+    catch (err) {
+        return c.json(
+            {
+                success: false,
+                msg: err instanceof Error ? err.message : responseMsg.generic.error.GENERIC_500, 
+                data: null
+            },
+            500,
+        );
+    }    
 }
