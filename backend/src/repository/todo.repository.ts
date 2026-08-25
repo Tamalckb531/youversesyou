3924c9870ca2473c896decb7e09ddb78a0959597
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:32:27 +0600

    add database level types

--- backend/src/repository/todo.repository.ts
+++ backend/src/repository/todo.repository.ts
@@ -2,6 +2,9 @@ import { eq } from "drizzle-orm";
 import { getDb } from "../db";
 import { todos } from "../db/schema";
 
+export type NewTodo = typeof todos.$inferInsert;
+export type Todo = typeof todos.$inferSelect;
+
 export const TodoRepository = {
     async findAllHabitsByUserId(userId: string) {
         return await getDb()
