c5c6c31c16e2ca94247ac63d813c4e771b4f1655
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:31:01 +0600

    add findAllHabitsByUserId

--- backend/src/repository/todo.repository.ts
+++ backend/src/repository/todo.repository.ts
@@ -1,3 +1,12 @@
-export const TodoRepository = {
+import { eq } from "drizzle-orm";
+import { getDb } from "../db";
+import { todos } from "../db/schema";
 
+export const TodoRepository = {
+    async findAllHabitsByUserId(userId: string) {
+        return await getDb()
+            .select()
+            .from(todos)
+            .where(eq(todos.userId, userId));
+    },
 }
