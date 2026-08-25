493d6ba20a9b1cb9c1c1f0b913d9a420adf51d98
Tamal <ckbtamaldipnew@gmail.com>
2026-08-25 06:11:51 +0600

    add oneTodo

--- backend/src/service/todo.service.ts
+++ backend/src/service/todo.service.ts
@@ -1,6 +1,13 @@
+import { responseMsg } from "../lib/constants";
 
 export const TodoService = {
     async allTodos(userId: string) {
         return TodoRepository.findAllHabitsByUserId(userId);  
+    },
+
+    async oneTodo(userId: string, habitId: string) {
+        const todo = await TodoRepository.oneTodo(habitId, userId);
+        if (!todo) throw new Error(responseMsg.todo.error.NO_TODO_ID);
+        return todo;
     },
 }
